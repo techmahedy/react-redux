6b6c736f1cb1b113a3e2dc90f6dbe149613182f9
techmahedy <mahedy150101@gmail.com>
2021-04-18 12:39:16 +0600

    useSelector, useDispatch

--- src/App.js
+++ src/App.js
@@ -1,40 +1,19 @@
 import './App.css';
-import update_person from './store/actions/personAction';
-import update_game from './store/actions/gameAction';
 import fetch_user from './store/actions/userAction';
-import { connect } from 'react-redux';
+import { connect, useSelector, useDispatch} from 'react-redux';
 
 function App(props) {
+  const users = useSelector(state => state.users);
+  const dispatch = useDispatch();
   return (
     <div className="App">
       <h1>Redux Tutorial</h1>
-      Person Name: {props.person.name}
-      <button onClick={props.updatePerson}>Update Person</button>
-      <br/>
-      Game Name: {props.game.name}
-      <button onClick={props.updateGame}>Update Game</button>
-      <br/>
-      Users: <button onClick={props.fetchUsers}>Fetch Users</button>
+      Users: <button onClick={()=>dispatch(fetch_user)}>Fetch Users</button>
       {
-        props.users.length === 0 ? <p>No user found!</p> :
-        props.users.map(user => <p key={user.id}>{user.first_name}</p>) 
+        users.length === 0 ? <p>No user found!</p> :
+        users.map(user => <p key={user.id}>{user.first_name}</p>) 
       }
     </div>
   );
 }
-const mapStateToProps = state => {
-    return {
-      game: state.game,
-      person: state.person,
-      users: state.users
-    };
-}
-
-const mapDispatchToProps = dispatch => {
-  return {
-    updateGame: () => {dispatch(update_game)},
-    updatePerson: () => {dispatch(update_person)},
-    fetchUsers: () => {dispatch(fetch_user)} 
-  }
-}
-export default connect(mapStateToProps,mapDispatchToProps)(App);
+export default App;
